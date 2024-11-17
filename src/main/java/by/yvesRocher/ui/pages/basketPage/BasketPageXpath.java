9e2dfe8233891f56a1a59cd2c09116d0bffab111
Arseni Aleksandrovich <theintosky@gmail.com>
2024-11-17 16:24:19 +0300

    add BasketPageXpath class

--- src/main/java/by/yvesRocher/ui/pages/basketPage/BasketPageXpath.java
+++ src/main/java/by/yvesRocher/ui/pages/basketPage/BasketPageXpath.java
@@ -1,4 +1,7 @@
 package by.yvesRocher.ui.pages.basketPage;
 
-public class BasketPageXpath {
+public class BasketPageXpath {private static final String CONTINUE_BUTTON_LOCATOR_XPATH =
+        "//button[@class='basket-btn eye-button ng-star-inserted']";
+    private static final String DELETE_PRODUCT_FROM_BASKET_LOCATOR_XPATH =
+            "//div[@class='delete-btn-and-price']/button[@type='button']";
 }
