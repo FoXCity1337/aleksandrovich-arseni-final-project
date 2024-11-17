c63ad1e2cec60afb57d9ba329ff9b15f8a033683
Arseni Aleksandrovich <theintosky@gmail.com>
2024-11-17 15:58:38 +0300

    add HomePageXpath class with BASKET_LOCATOR_XPATH and LOGIN_LOCATOR_XPATH

---
+++ src/main/java/by/yvesRocher/ui/pages/homePage/HomePageXpath.java
@@ -0,0 +1,6 @@
+package by.yvesRocher.ui.pages.homePage;
+
+public class HomePageXpath {
+    private static final String BASKET_LOCATOR_XPATH = "//li/a[@href='/basket']";
+    private static final String LOGIN_LOCATOR_XPATH = "//li/a[@href='/auth/login']";
+}
