06f4328e0dbe7d10a296549e9f0c4b6c632c0385
Arseni Aleksandrovich <theintosky@gmail.com>
2024-11-17 16:29:49 +0300

    add BasketPage class with clickDeleteProduct() and clickContinueButton() methods

---
+++ src/main/java/by/yvesRocher/ui/pages/basketPage/BasketPage.java
@@ -0,0 +1,27 @@
+package by.yvesRocher.ui.pages.basketPage;
+
+import by.yvesRocher.ui.driver.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class BasketPage {
+    private WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public BasketPage(WebDriver driver) {
+        this.driver = Driver.getDriver();
+    }
+
+    public BasketPage clickDeleteProduct(){
+        getDriver().findElement(By.xpath(BasketPageXpath.DELETE_PRODUCT_FROM_BASKET_LOCATOR_XPATH));
+        return this;
+    }
+
+    public BasketPage clickContinueButton(){
+        getDriver().findElement(By.xpath(BasketPageXpath.CONTINUE_BUTTON_LOCATOR_XPATH));
+        return this;
+    }
+}
--- src/main/java/by/yvesRocher/ui/pages/basketPage/BasketPageXpath.java
+++ src/main/java/by/yvesRocher/ui/pages/basketPage/BasketPageXpath.java
@@ -1,7 +1,8 @@
 package by.yvesRocher.ui.pages.basketPage;
 
-public class BasketPageXpath {private static final String CONTINUE_BUTTON_LOCATOR_XPATH =
+public class BasketPageXpath {
+    public static final String CONTINUE_BUTTON_LOCATOR_XPATH =
         "//button[@class='basket-btn eye-button ng-star-inserted']";
-    private static final String DELETE_PRODUCT_FROM_BASKET_LOCATOR_XPATH =
+    public static final String DELETE_PRODUCT_FROM_BASKET_LOCATOR_XPATH =
             "//div[@class='delete-btn-and-price']/button[@type='button']";
 }
